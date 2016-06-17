84300dc89500e0129f798c414ea86ad44f8cebe7
damantou <276921727@qq.com>
2016-06-17 09:52:11 +0800

    Create Guard.php

---
+++ Guard.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @name StopAttack
+ * @author ManTou<dadademantou@gmail.com>
+ */
+class Guard{
+    
+    private static $getFilter    = "\\<.+javascript:window\\[.{1}\\\\x|<.*=(&#\\d+?;?)+?>|<.*(data|src)=data:text\\/html.*>|\\b(alert\\(|confirm\\(|expression\\(|prompt\\(|benchmark\s*?\(.*\)|sleep\s*?\(.*\)|load_file\s*?\\()|<[a-z]+?\\b[^>]*?\\bon([a-z]{4,})\s*?=|^\\+\\/v(8|9)|\\b(and|or)\\b\\s*?([\\(\\)'\"\\d]+?=[\\(\\)'\"\\d]+?|[\\(\\)'\"a-zA-Z]+?=[\\(\\)'\"a-zA-Z]+?|>|<|\s+?[\\w]+?\\s+?\\bin\\b\\s*?\(|\\blike\\b\\s+?[\"'])|\\/\\*.*\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT\s*(\(.+\)\s*|@{1,2}.+?\s*|\s+?.+?|(`|'|\").*?(`|'|\")\s*)|UPDATE\s*(\(.+\)\s*|@{1,2}.+?\s*|\s+?.+?|(`|'|\").*?(`|'|\")\s*)SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE)@{0,2}(\\(.+\\)|\\s+?.+?\\s+?|(`|'|\").*?(`|'|\"))FROM(\\(.+\\)|\\s+?.+?|(`|'|\").*?(`|'|\"))|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)";
+    private static $postFilter   = "<.*=(&#\\d+?;?)+?>|<.*data=data:text\\/html.*>|\\b(alert\\(|confirm\\(|expression\\(|prompt\\(|benchmark\s*?\(.*\)|sleep\s*?\(.*\)|load_file\s*?\\()|<[^>]*?\\b(onerror|onmousemove|onload|onclick|onmouseover)\\b|\\b(and|or)\\b\\s*?([\\(\\)'\"\\d]+?=[\\(\\)'\"\\d]+?|[\\(\\)'\"a-zA-Z]+?=[\\(\\)'\"a-zA-Z]+?|>|<|\s+?[\\w]+?\\s+?\\bin\\b\\s*?\(|\\blike\\b\\s+?[\"'])|\\/\\*.*\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT\s*(\(.+\)\s*|@{1,2}.+?\s*|\s+?.+?|(`|'|\").*?(`|'|\")\s*)|UPDATE\s*(\(.+\)\s*|@{1,2}.+?\s*|\s+?.+?|(`|'|\").*?(`|'|\")\s*)SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE)(\\(.+\\)|\\s+?.+?\\s+?|(`|'|\").*?(`|'|\"))FROM(\\(.+\\)|\\s+?.+?|(`|'|\").*?(`|'|\"))|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)";
+    private static $cookieFilter = "benchmark\s*?\(.*\)|sleep\s*?\(.*\)|load_file\s*?\\(|\\b(and|or)\\b\\s*?([\\(\\)'\"\\d]+?=[\\(\\)'\"\\d]+?|[\\(\\)'\"a-zA-Z]+?=[\\(\\)'\"a-zA-Z]+?|>|<|\s+?[\\w]+?\\s+?\\bin\\b\\s*?\(|\\blike\\b\\s+?[\"'])|\\/\\*.*\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT\s*(\(.+\)\s*|@{1,2}.+?\s*|\s+?.+?|(`|'|\").*?(`|'|\")\s*)|UPDATE\s*(\(.+\)\s*|@{1,2}.+?\s*|\s+?.+?|(`|'|\").*?(`|'|\")\s*)SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE)@{0,2}(\\(.+\\)|\\s+?.+?\\s+?|(`|'|\").*?(`|'|\"))FROM(\\(.+\\)|\\s+?.+?|(`|'|\").*?(`|'|\"))|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)";
+    
+    /**
+     * 检查
+     * @return boolean
+     */
+    public static function run(){
+        if(!self::stop($_GET,self::$getFilter))                      return false;
+        parse_str(file_get_contents("php://input"), $_PUT);
+        if(!self::stop(array_merge($_POST,$_PUT),self::$postFilter)) return false;
+        if(!self::stop($_COOKIE,self::$cookieFilter))                return false;
+        if(!self::stop($_SERVER['HTTP_REFERER'],self::$postFilter))  return false;
+        return true;
+    }
+    
+    /**
+     * 检查
+     * @param unknown $data
+     * @param unknown $filter
+     */
+    private static function stop($data,$filter)
+    {
+        foreach ( $data as $k=>$v ){
+            $v = self::parseParams($v);
+            if (preg_match("/".$v."/is",$filter)==1){
+                return false;
+            }
+            if (preg_match("/".$k."/is",$filter)==1){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    /**
+     * 参数拆分
+     * @param unknown $arr
+     * @return unknown|string
+     */
+    private static function parseParams($array)
+    {
+        static $str;
+        static $keystr;
+        if (!is_array($array)) return $array;
+        foreach ($array as $key => $val ) {
+            $keystr=$keystr.$key;
+            if (is_array($val)) {
+                self::parseParams($val);
+            } else {
+                $str[] = $val.$keystr;
+            }
+        }
+        return implode($str);
+    }
+}
